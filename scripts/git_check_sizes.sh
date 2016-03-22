b8288761d67c30546c82292ad8a3a12e4c0a680b
Russell Weatherburn <russell.weatherburn@mediaware.com.au>
2016-03-23 10:55:51 +1100

    Correction of typo in comments

--- scripts/git_check_sizes.sh
+++ scripts/git_check_sizes.sh
@@ -7,7 +7,7 @@
 # @see http://stubbisms.wordpress.com/2009/07/10/git-script-to-show-largest-pack-objects-and-trim-your-waist-line/
 # @author Antony Stubbs
 
-# set the internal field spereator to line break, so that we can iterate easily over the verify-pack output
+# set the internal field separator to line break, so that we can iterate easily over the verify-pack output
 IFS=$'\n';
 
 # list all objects including their size, sort by size, take top 10
